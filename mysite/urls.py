172d52a60616b765f0577262035ca762a38dc652
jyotinayak1976 <jyotinayak1976@gmail.com>
2021-02-15 13:05:09 +0530

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',include('blog.urls',namespace='blog')),
-
+    path('',include('blog.urls',namespace='blog')),
 ]
 
 if settings.DEBUG:
